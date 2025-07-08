dc9797289e631e3408d4c91e9ef2c7c24e44580f
Kishan Rajagunathas <kishan.rajagunathas@pennentertainment.com>
2025-07-07 23:47:26 -0400

    setup db

--- twilio-backend/src/services/LLMConnection.ts
+++ twilio-backend/src/services/LLMConnection.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import path from 'path';
 import { OpenAIResponse } from '../types/Types';
 
-dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
 
 
 export async function callOpenAI(
