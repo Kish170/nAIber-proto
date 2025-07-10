e311c347509067156b2f9a40e53c7106bd29b288
Kishan Rajagunathas <kishan.rajagunathas@pennentertainment.com>
2025-07-10 01:43:18 -0400

    added tools and CRUD for basic info

--- twilio-backend/src/services/LLMConnection.ts
+++ twilio-backend/src/services/LLMConnection.ts
@@ -1,9 +1,15 @@
 import dotenv from 'dotenv';
 import path from 'path';
 import { OpenAIResponse } from '../types/Types';
+import { ChatOpenAI } from "@langchain/openai";
+import { MemorySaver } from "@langchain/langgraph";
+import { BaseMessage } from '@langchain/core/messages'
+import { ToolNode } from '@langchain/langgraph/prebuilt'
+import { createReactAgent } from "@langchain/langgraph/prebuilt";
+import { createUserTool, updateBasicInfoTool } from '../tools/BasicInfoTools';
+import { Annotation } from '@langchain/langgraph'
 
 dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
-
 
 export async function callOpenAI(
   userMessage: string,
@@ -11,6 +17,27 @@ export async function callOpenAI(
 ): Promise<OpenAIResponse> {
   const url = 'https://api.openai.com/v1/chat/completions';
   const apiKey = process.env.OPENAI_API_KEY;
+  const agentTools = [createUserTool, updateBasicInfoTool]
+  const AgentState = Annotation.Root({
+    messages: Annotation<BaseMessage[]>({
+      reducer: (x, y) => x.concat(y),
+    }),
+    userId: Annotation<string>(),
+    currentStep: Annotation<string>(),
+    collectedData: Annotation<any>()
+  })
+  const toolNode = new ToolNode<typeof AgentState.State>(agentTools);
+  const agentCheckpointer = new MemorySaver();
+  const agentModel = new ChatOpenAI({
+    model: "gpt-4o-mini",
+    temperature: 0
+  });
+
+  const agent = createReactAgent({
+    llm: agentModel,
+    tools: agentTools,
+    checkpointer: agentCheckpointer
+  })
 
   if (!apiKey) {
     console.error('OpenAI API key is not defined in environment variables.');
